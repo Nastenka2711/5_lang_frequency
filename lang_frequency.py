caa17eb93bf2b3c547cb41d03c99bc7ed138f2d0
Nastenka2711 <Nastasya.saurova@gmail.com>
2016-10-04 22:36:11 +0400

    Update lang_frequency.py
    Не могу понять, почему, но он всё равно считает какие-то пустые символы за слова 0_0

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,7 +1,10 @@
 import collections
 import re
+import os
+import sys
 
 c = collections.Counter()
+
 
 def load_data(filepath):
     for line in filepath:
@@ -9,12 +12,20 @@ def load_data(filepath):
         get_most_frequent_words(words)
     return
 
+
 def get_most_frequent_words(text):
     for word in text:
         word = word.lower()
         c[word] += 1
 
-if __name__ == '__main__':
-    load_data(open('list_words.txt','r'))
-    print(c.most_common(10))
 
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        name_file = input("Введите название файла  ")
+    else:
+        name_file = sys.argv[1]
+    if os.path.exists(name_file) and os.path.isfile(name_file):
+        load_data(open(name_file, 'r'))
+        print(c.most_common(10))
+    else:
+        print("Ошибка открытия файла")

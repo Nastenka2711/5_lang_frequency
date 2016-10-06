cae43e059ebb5a4b23e473ce7623b9583b730aff
Nastenka2711 <Nastasya.saurova@gmail.com>
2016-10-06 19:25:57 +0400

    Update lang_frequency.py
    Исправила: 1) переменную final_list_words убрала из глобальных (она вообще раньше по-другому называлась)
                        2) добавила функции проверки существования файла, ввода названия файла при его отсутствии
                        3) сделала более понятные названия функций и переменых (ну как мне кажется, во всяком случае)
    Вопрос: Я могу вспомогательную переменную назвать буквой? Ибо она встречается один раз и писать о том что она вспомогательная - добавлять смысловую нагрузку читающему код. Нет?
                       4) исправила READ.ME

--- lang_frequency.py
+++ lang_frequency.py
@@ -3,32 +3,42 @@ import re
 import os
 import sys
 
-c = collections.Counter()
 
-
-def load_data(filepath):
-    for line in filepath:
-        if len(line) > 1:
-            line = line.lower()
-            p = re.compile(r'\W+')
-            words = p.split(line)
-            get_most_frequent_words(words)
-    return
-
-
-def get_most_frequent_words(text):
-    for word in text:
-        word = word.lower()
-        c[word] += 1
-
-
-if __name__ == '__main__':
+def input_name_file(line_arguments):
     if len(sys.argv) == 1:
         name_file = input("Введите название файла  ")
     else:
         name_file = sys.argv[1]
+    return name_file
+
+
+def existence_file(filepath):
     if os.path.exists(name_file) and os.path.isfile(name_file):
-        load_data(open(name_file, 'r'))
-        print(c.most_common(10))
+        return 0
     else:
+        return None
+
+
+def treatment_data(filepath):
+    final_list_words = collections.Counter()
+    for line in filepath:
+        if len(line) > 1:
+            line = line.lower()
+            subsidiary_regular_object = re.compile(r'\W+')
+            words_from_line = subsidiary_regular_object.split(line)
+            get_most_frequent_words(words_from_line, final_list_words)
+    print(final_list_words.most_common(10))
+
+
+def get_most_frequent_words(text_line, final_list_words):
+    for word in text_line:
+        final_list_words[word] += 1
+    return(final_list_words)
+
+
+if __name__ == '__main__':
+    name_file = input_name_file(sys.argv)
+    if existence_file(name_file) is None:
         print("Ошибка открытия файла")
+    else:
+        treatment_data(open(name_file, 'r'))

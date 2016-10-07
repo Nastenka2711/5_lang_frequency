4631b484b60163a0a47897c1b9a4bbfc39e50257
Nastenka2711 <Nastasya.saurova@gmail.com>
2016-10-07 19:58:00 +0400

    Update lang_frequency.py
    Have used "collections.Counter" before but changed method and removed useless function. Fixed bug associated with magical numbers. I renamed variables. My English is very bad, sorry.

--- lang_frequency.py
+++ lang_frequency.py
@@ -4,41 +4,38 @@ import os
 import sys
 
 
-def input_name_file():
+def input_file_name():
     if len(sys.argv) == 1:
-        name_file = input("Введите название файла  ")
+        file_name = input("Введите название файла  ")
     else:
-        name_file = sys.argv[1]
-    return name_file
+        file_name = sys.argv[1]
+    return file_name
 
 
-def existence_file(filepath):
-    if os.path.exists(name_file) and os.path.isfile(name_file):
+def file_exists(filepath):
+    if os.path.exists(filepath) and os.path.isfile(filepath):
         return 0
-    else:
-        return None
 
 
-def treatment_data(filepath):
-    final_list_words = collections.Counter()
-    for line in filepath:
-        if len(line) > 1:
-            line = line.lower()
-            subsidiary_regular_object = re.compile(r'\W+')
-            words_from_line = subsidiary_regular_object.split(line)
-            get_most_frequent_words(words_from_line, final_list_words)
-    print(final_list_words.most_common(10))
+def get_words_from_line(line):
+    words_from_line = []
+    if len(line) > 1:
+        line = line.lower()
+        words_from_line = re.findall(r'[^\W0-9_]+', line)
+    return words_from_line
 
 
-def get_most_frequent_words(text_line, final_list_words):
-    for word in text_line:
-        final_list_words[word] += 1
-    return(final_list_words)
+def treatment_data(file_data, word_amount):
+    word_list = collections.Counter()
+    for line in file_data:
+            word_list.update(get_words_from_line(line))
+    print(word_list.most_common(word_amount))
 
 
 if __name__ == '__main__':
-    name_file = input_name_file()
-    if existence_file(name_file) is None:
+    word_amount = 10
+    file_name = input_file_name()
+    if file_exists(file_name) is None:
         print("Ошибка открытия файла")
     else:
-        treatment_data(open(name_file, 'r'))
+        treatment_data(open(file_name, 'r'), word_amount)
